25c587846b80e1643162d312aff5dc0630863f32
1Plushkin <Plushkin001@mail.ru>
2025-10-27 16:05:38 +0500

    3

---
+++ Tyuiu.KugaevskijSD.Sprint3.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KugaevskijSD.Sprint3.Task3.V10.Lib;
+namespace Tyuiu.KugaevskijSD.Sprint3.Task3.V10.Test;
+
+[TestClass]
+public sealed class DataServiceTest
+{
+    [TestMethod]
+    public void ValidDeleteCharInString()
+    {
+        DataService ds = new DataService();
+        string value = "gdfppf vfppt p";
+        char item = 'p';
+        string res = ds.DeleteCharInString(value, item);
+        Assert.AreEqual("gdff vft ", res);
+    }
+}
