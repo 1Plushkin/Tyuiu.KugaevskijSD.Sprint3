9f531be027d235e26d760a2c9a9fe2f4a58c6489
1Plushkin <Plushkin001@mail.ru>
2025-10-27 14:42:42 +0500

    Add project files.

---
+++ Tyuiu.KugaevskijSD.Sprint3.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿namespace Tyuiu.KugaevskijSD.Sprint3.Task0.V1.Test;
+using Tyuiu.KugaevskijSD.Sprint3.Task0.V1.Lib;
+
+[TestClass]
+public class DataServiceTest
+{
+    [TestMethod]
+    public void ValidGetSumSeries()
+    {
+        DataService ds = new DataService();
+
+        int value = 4;
+        int startValue = 1;
+        int stopValue = 10;
+
+        double res = ds.GetSumSeries(value, startValue, stopValue);
+        double wait = 890;
+        Assert.AreEqual(wait, res);
+    }
+}
